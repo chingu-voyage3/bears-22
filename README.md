2b5a1eb4d81ef8354dfc0e68ad65e5ff27e6d8cf
Philip <philip.balbas@gmail.com>
2017-12-17 10:00:34 +0800

    integrated SCSS

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # bears-22
+
+## Instructions
+
+❗️ You must have the latest version of Node and NPM to run this project
+
+Please download it from here: nodejs.org/
+
+    npm i
+    npm start
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Bears-22 Project</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
