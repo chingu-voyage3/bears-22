ceca7adddb7650a665a7558e64ba0a0db12ace3c
Philip <philip.balbas@gmail.com>
2017-12-17 12:58:02 +0800

    setup fake data

--- server/app.js
+++ server/app.js
@@ -1,15 +1,19 @@
 // Imports
 const express = require('express');
-const api = require('./routes/api');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
+const api = require('./routes/api');
 // const keys = require('./config/keys');
 const data = require('./data.json');
 
 // Set up express app
 const app = express();
 
+app.use(cors());
+
 //Connect to mongodb
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 // const connection = mongoose
 //   .connect(keys.mongodb.dbURI, {
 //     useMongoClient: true
@@ -22,13 +26,16 @@ mongoose.Promise = global.Promise;
 // app.use(express.static('public')); // Serve static files
 
 // Set up routes
-app.use('/api', api);
 
-app.get('/user', (req, res) => {
+const PORT = process.env.port || 8080;
+
+app.get('/users', (req, res) => {
   res.send(data);
 });
 
+// app.use('/api', api);
+
 // Listen for requests on port 5000
-app.listen(process.env.port || 5000, () => {
-  console.log('Listening on port 5000... ');
+app.listen(PORT, () => {
+  console.log(`Sever running at http://localhost:${PORT} ... `);
 });
