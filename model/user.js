fe309a2976b42a2df7430f13f94b18cb09100e02
Philip <philip.balbas@gmail.com>
2017-12-17 10:09:17 +0800

    setup testing

--- model/user.js
+++ model/user.js
@@ -4,12 +4,11 @@ const Schema = mongoose.Schema;
 
 // create user schema and model
 const UserSchema = new Schema({
-    name: {
-        type: String,
-        required: [true, 'Name field is required']
-    }
+  name: {
+    type: String,
+    required: [ true, 'Name field is required' ]
+  }
 });
 const User = mongoose.model('user', UserSchema);
-
 // Export models
 module.exports = User;
