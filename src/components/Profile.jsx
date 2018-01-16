56f010b1296e5aaf75c3205c55562c83a041aa63
Jordan Johnson <jordanleo7@hotmail.com>
2018-01-16 12:11:54 -0600

    Added gql query data to profile elements

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
-import u14 from './temp_assets/u14.png';
+import u14 from './temp_assets/u14.png'
 
 const profileQuery = gql`
-query {
-    user (user_id: 1) {
+  query {
+    user(user_id: 1) {
       id
       email
       username
@@ -24,16 +24,16 @@ query {
       bio
       country {
         name
-      },
+      }
       city {
         name
       }
-    skills {
-      id
-      name
+      skills {
+        id
+        name
+      }
     }
   }
-}
 `
 
 const Profile = ({ userInfo, data: { user, refetch, error, loading } }) => {
@@ -54,14 +54,23 @@ const Profile = ({ userInfo, data: { user, refetch, error, loading } }) => {
           {/*Image and Basic Info*/}
           <section className="profile__bg-grey row">
             <div className="col-4">
-              <img src={u14} alt="Profile Image" className="profile__image"/>
+              <img src={u14} alt="Profile Image" className="profile__image" />
             </div>
             <div className="profile__header-text col-8">
-              <h3>{user.first_name} {user.last_name}</h3>
-              <p>{user.city}, {user.country}</p>
+              <h3>
+                {user.first_name} {user.last_name}
+              </h3>
+              <p>
+                {user.city}, {user.country}
+              </p>
               <a href={user.portfolio_url}>{user.portfolio_url}</a>
             </div>
-            <a href={'mailto:' + user.email} className="profile__button-contact">Contact</a>
+            <a
+              href={'mailto:' + user.email}
+              className="profile__button-contact"
+            >
+              Contact
+            </a>
           </section>
           {/*About and Skills*/}
           <section className="profile__bg-black row">
