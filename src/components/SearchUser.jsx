d1e5b36d1b80e425995aa169c5c203fc170f5a22
snowleo208 <13027432+snowleo208@users.noreply.github.com>
2018-01-22 23:01:30 +0800

    fixed styles for user search page

--- src/components/SearchUser.jsx
+++ src/components/SearchUser.jsx
@@ -1,35 +1,31 @@
 import React, { Component } from 'react'
 
 class SearchUser extends Component {
-  constructor(props) {
-    super(props)
-  }
-
   render() {
     const { data } = this.props;
     if(this.props.search === 'users') {
       return (
-        <div>
+         <div className="search__user__grid">
         {!data.loading ? (data[this.props.search] && 
           data[this.props.search]
           .filter(item => item.first_name.match(this.props.selectedItem))
           .map((item, index) => (
-            <div className="search__user d-flex flex-column">
-              <div className="d-flex flex-row search__user__header">
-                <span className="search__thumbnail" key={index}>{item.first_name.slice(0,1) }</span>
-                <div>
-                <span className="search__result__title">{item.first_name + ' ' + item.last_name}</span>
-                 <div>
-                <ul className="list__inline list__border">
-                  <li>Node.js</li>
-                  <li>Express.js</li>
-                  <li>Back End</li>
-                </ul>
-                </div>
+            <div className="search__user d-flex flex-column" key={this.props.search + '-' + index}>
+                <div className="d-flex flex-row align-items-center justify-content-center">
+                    <span className="search__thumbnail" >{item.first_name.slice(0,1) }</span>
+                  <div className="search__title__grid">
+                    <span className="search__result__title">{item.first_name + item.last_name}</span>
+                     <div>
+                    <ul className="list__inline list__border">
+                      <li>Node.js</li>
+                      <li>Express.js</li>
+                      <li>Back End</li>
+                    </ul>
+                    </div>
                 </div>
               </div>
               <div className="search__desc">
-              {item.bio ? item.bio : 'This user does not have any self introduction.' + ' (View More...)'}
+              {item.bio ? item.bio : 'This user does not have any self introductions.'}
               </div>
             </div>))) : (
         <div>Loading...</div>
@@ -43,18 +39,18 @@ class SearchUser extends Component {
           data[this.props.search]
           .filter(item => item.title.match(this.props.selectedItem))
           .map((item, index) => (
-            <div className="search__user d-flex flex-column">
-              <div className="d-flex flex-row align-items-center justify-content-center">
-                <span className="search__thumbnail" key={index}>{item.title.slice(0,1) }</span>
-                <div>
-                <span className="search__result__title">{item.title}</span>
-                 <div>
-                <ul className="list__inline list__border">
-                  <li>Node.js</li>
-                  <li>Express.js</li>
-                  <li>Back End</li>
-                </ul>
-                </div>
+            <div className="search__user d-flex flex-column" key={this.props.search + '-' + index}>
+                <div className="d-flex flex-row align-items-center justify-content-center">
+                    <span className="search__thumbnail" >{item.title.slice(0,1) }</span>
+                  <div className="search__title__grid">
+                    <span className="search__result__title">{item.title}</span>
+                     <div>
+                    <ul className="list__inline list__border">
+                      <li>Node.js</li>
+                      <li>Express.js</li>
+                      <li>Back End</li>
+                    </ul>
+                    </div>
                 </div>
               </div>
               <div className="search__desc">
