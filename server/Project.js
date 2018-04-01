6557f2087de85b86a3fa54a7577bf607c06de3c0
Jordan Johnson <jordanleo7@hotmail.com>
2018-04-01 02:30:15 -0500

    Added Project Model

---
+++ server/Project.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const projectSchema = new Schema({
+  title: String,
+  description: String,
+  skills: Array,
+  help: Boolean,
+  users: [{ type: ObjectId, ref: 'User' }]
+})
+
+module.exports = mongoose.model('project', userSchema)
